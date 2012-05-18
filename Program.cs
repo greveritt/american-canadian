9962ae36e1bb0991b4bcf96e61ef849459d44f49
Gregory R. Everitt <geveritt@drew.edu>
2012-05-17 21:40:51 -0400

    Comments are added, commented code is removed, and String output is no longer initialized as null.

--- Program.cs
+++ Program.cs
@@ -1,39 +1,39 @@
-﻿using System;
+﻿/* 
+ * American to Canadian translator by Gregory R. Everitt, Copyright 2012
+ * Converts American English to Canadian English using ground-breaking natural language recognition algorithms.
+ */
+using System;
 namespace CanadianTranslator
 {
     class Canadian 
     {
+        /*
+         * Copies contents of standard input into new variable removes all punctuation at the end of the sentence,
+         * then then concatenates ", eh?" to end of string. 
+         */
         static void Main() 
         {
             Console.WriteLine("What would you like me to translate for you?");
             String input = Console.ReadLine();
-            String output = null;
-            //if (input.EndsWith(".") || input.EndsWith("?") || input.EndsWith("!"))
-            //{
-            //    Console.WriteLine("Bad input, brah. Can't make this into Canadian, dawg, without a bit of preprocessing. Be right back, dude. American slang endearment terms.");
-            //    output = input;
-            //    output = output.Substring(0, output.Length - 1);
-            //    output = String.Concat(output, ", eh?");
-            //    Console.WriteLine(output);
-            //}
-            output = RemovePunctuation(input);
-            //else
-            //{
-                output = String.Concat(output, ", eh?");
-                Console.WriteLine(output);
-            //}
+            String output = RemovePunctuation(input);
+            output = String.Concat(output, ", eh?");
+            Console.WriteLine(output);
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
 
+        /*
+         * If String unpruned doesn't end with a period, quotation mark or exclamation point, then it returns that string.
+         * If it does end with one of the above punctuation marks, then punctuation is removed from string and method calls itself
+         * to see if there is any remaining punctuation at the end.
+         */
         static String RemovePunctuation(String unpruned)
         {
             String pruned = unpruned;
             if (pruned.EndsWith(".") || pruned.EndsWith("?") || pruned.EndsWith("!"))
             {
-                //Console.WriteLine("Bad input, brah. Can't make this into Canadian, dawg, without a bit of preprocessing. Be right back, dude. American slang endearment terms.");
                 pruned = pruned.Substring(0, pruned.Length - 1);
                 pruned = RemovePunctuation(pruned);
                 return pruned;
